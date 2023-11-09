6697664e682a51d63324ed0d9eef7ca153690484
dntjd129 <dntjd129@naver.com>
2023-11-09 15:18:49 +0900

    feat: db 연결, model 추가

---
+++ project/model/Mess.js
@@ -0,0 +1,46 @@
+function Message(Sequelize, DataTypes) {
+    return Sequelize.define(
+      "messages",
+      {
+        message_id: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          defaultValue: Sequelize.UUIDV4,
+        },
+        title: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+        },
+        content: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        is_public: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+          defaultValue: true, // 메시지를 기본적으로 공개로 만들기
+        },
+        message_at: {
+          type: DataTypes.DATETIME,
+          allowNull: false,
+        },
+        flower_color: {
+          type: DataTypes.STRING(10),
+          allowNull: false,
+        },
+        // 외래 키 참조 정의
+        id: {
+          type: DataTypes.BIGINT,
+          allowNull: false,
+        },
+      },
+      {
+        tableName: "messages",
+        freezeTableName: true, // 복수 형태 방지
+        timestamps: false,
+      }
+    );
+  }
+  
+  module.exports = Message;
+  
---
+++ project/model/User.js
@@ -0,0 +1,55 @@
+function User(Sequelize, DataTypes) {
+    return Sequelize.define(
+      "Users",
+      {
+        id: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          defaultValue: Sequelize.UUIDV4,
+        },
+        user_id: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        user_pw: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        user_pw_salt: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+        },
+        user_name: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+        },
+        sign_in_at: {
+          type: DataTypes.DATETIME,
+          allowNull: false,
+        },
+        user_intro_self: {
+          type: DataTypes.STRING(255),
+          defaultValue: "",
+        },
+        user_mbti: {
+          type: DataTypes.STRING(4),
+          allowNull: false,
+        },
+        user_messages: {
+          type: DataTypes.BIGINT,
+          defaultValue: 0,
+        },
+        user_img: {
+          type: DataTypes.STRING(255),
+        },
+      },
+      {
+        tableName: "Users",
+        freezeTableName: true, //복수 형태 방지
+        timestamps: false,
+      }
+    );
+  }
+  
+  module.exports = User;
+  
