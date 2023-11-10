eb127badeeeabb37a2988c710bf59bbe5d04b992
dntjd129 <dntjd129@naver.com>
2023-11-10 16:25:40 +0900

    feat: 암호화 추가

--- project/controller/Chome.js
+++ project/controller/Chome.js
@@ -1,4 +1,6 @@
-const { User, Message } = require('../model')
+const { User, Message } = require("../model")
+const pwSalt = require("../model/pwSalt")
+
 // 메인 페이지 랜더
 exports.home = (req, res) => {
   res.render("home")
@@ -10,17 +12,47 @@ exports.signIn = (req, res) => {
 }
 
 // '로그인' 버튼 클릭 시
-exports.signInUser = (req, res) => {
+// exports.signInUser = (req, res) => {
+//   User.findOne({
+//     where: {
+//       user_id: req.body.user_id, 
+//       user_pw: req.body.user_pw
+//     }
+//   }).then((result) => {
+//     console.log("signInUser : ", result)
+//     if(result) {
+//       res.send({result: true})
+//     } else res.send({result: false})
+//   })
+// }
+
+// 로그인 버튼 클릭 시 (crypto 추가 버전)
+exports.singInUser = (req, res) => {
   User.findOne({
     where: {
-      user_id: req.body.user_id, 
-      user_pw: req.body.user_pw
+      user_id: req.body.user_id
     }
   }).then((result) => {
-    console.log("signInUser : ", result)
     if(result) {
-      res.send({result: true})
-    } else res.send({result: false})
+      pwSalt.comparePassword(req.body.user_pw, user.pwsalt, user.pw)
+      .then(isPasswordCorrect => {
+        if(isPasswordCorrect) {
+          console.log("signInUser : ", result)
+          res.send({ result: true })
+        } else {
+          res.send({ result: false })
+        }
+      })
+      .catch(error => {
+        console.error("pw error : ", error);
+        res.send({ result: false })
+      })
+    } else {
+      res.send({ result: false })
+    }
+  }).catch(error => {
+    console.error("no user : ", error)
+    res.send({ result: false })
   })
 }
 
@@ -49,10 +81,37 @@ exports.idCheck = (req, res) => {
 }
 
 // '회원가입' 버튼 클릭 시
+// exports.signUpUser = (req, res) => {
+//   User.create(req.body).then((result) => {
+//     console.log("signupUser : ", result)
+//     res.send({result : true})
+//   })
+// }
+
+// 회원가입 버튼 클릭 시
 exports.signUpUser = (req, res) => {
-  User.create(req.body).then((result) => {
-    console.log("signupUser : ", result)
-    res.send({result : true})
+  const { user_id, user_pw, user_name, user_intro_self, user_mbti } = req.body;
+
+  pwSalt.hashPassword(user_pw)
+  .then(({ hashedPw, salt }) => {
+    User.create({
+      user_id: user_id,
+      user_pw: hashedPw,
+      user_pw_salt: salt,
+      user_name: user_name,
+      user_intro_self: user_intro_self,
+      user_mbti: user_mbti
+    }).then((result) => {
+      console.log("signUpUser", result);
+      res.send({ result: true })
+    }).catch((error) => {
+      console.error("user 생성 에러", error)
+      res.send({ result: false })
+    })
+  })
+  .catch((error) => {
+    console.error("암호화 에러", error)
+    res.send({ result: false })
   })
 }
 
