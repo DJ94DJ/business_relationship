be29238b3d56d30f04761e43863926d0c3293e16
yeeeeechan <leeyechan6@gmail.com>
2023-11-13 18:41:23 +0900

    feat: 프로필 페이지 랜더, 비번 수정(진행 중)

--- project/routes/user.js
+++ project/routes/user.js
@@ -5,8 +5,14 @@ const router = express.Router();
 // 마이페이지
 router.get('/profile', controller.profile);
 
-// 마이페이지 회원 정보 수정
+// 마이페이지 비번 수정 시 인증
+router.post('/profile', controller.searchUser);
+
+// 마이페이지 회원 정보(닉네임, 자기소개, mbti) 수정
 router.patch('/profile', controller.editUser);
+
+// 마이페이지 비번 수정
+router.patch('/profile/pw', controller.editPw);
 
 // 마이페이지 회원 탈퇴
 router.delete('/profile', controller.deleteUser);
